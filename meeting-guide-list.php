e8160d3239610eb4a325629eb853fe343cd6afa7
Ron Raney <raneyron@yahoo.com>
2019-03-22 08:34:34 -0500

    Create meeting-guide-list.php
    This was created in the web browser

---
+++ meeting-guide-list.php
@@ -0,0 +1,12 @@
+<?php
+   /*
+   Plugin Name: 12 Step Meeting Guide List
+   Plugin URI: http://github.com/ronraney/meeting-guide-list
+   description: >-
+  a plugin that works with 12 Step Meeting Guide plugin data feeds to display meetings as a widget
+   Version: 1.0
+   Author: Ron Raney
+   Author URI: http://ronraney.com
+   License: GPL2
+   */
+?>
